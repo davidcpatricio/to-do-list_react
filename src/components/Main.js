d727bf95a55dae9e18e3b911f0d13b08aa0bd095
David Patrício <david.c.patricio@gmail.com>
2023-12-28 17:00:00 +0000

    Adding tasks to list

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,7 +11,21 @@ import './Main.css';
 export default class Main extends Component {
   state = {
     newTask: '',
-    tasks: ['Make coffee', 'Drink water', 'Study'],
+    tasks: [],
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { tasks } = this.state;
+    let { newTask } = this.state;
+    newTask = newTask.trim();
+
+    if (tasks.indexOf(newTask) !== -1) return;
+
+    const newTasks = [...tasks];
+    this.setState({
+      tasks: [...newTasks, newTask],
+    });
   };
 
   handleChange = (e) => {
@@ -27,7 +41,7 @@ export default class Main extends Component {
       <div className="main">
         <h1>To-Do List</h1>
 
-        <form action="#" className="form">
+        <form onSubmit={this.handleSubmit} action="#" className="form">
           <input onChange={this.handleChange} type="text" value={newTask} />
           <button type="submit">
             <FaPlus />
@@ -38,10 +52,10 @@ export default class Main extends Component {
           {tasks.map((task) => (
             <li key={task}>
               {task}
-              <div>
+              <span>
                 <FaEdit className="edit" />
                 <FaWindowClose className="delete" />
-              </div>
+              </span>
             </li>
           ))}
         </ul>
