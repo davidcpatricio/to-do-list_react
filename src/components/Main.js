04187807894eaac81090138a2ef6646c085bfe84
David Patrício <david.c.patricio@gmail.com>
2023-12-28 15:11:16 +0000

    Stylised form

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+
+// Form
+import { FaPlus } from 'react-icons/fa';
+
 import './Main.css';
 
 export default class Main extends Component {
@@ -13,13 +17,17 @@ export default class Main extends Component {
   };
 
   render() {
+    const { newTask } = this.state;
+
     return (
       <div className="main">
         <h1>To-Do List</h1>
 
-        <form action="#">
-          <input onChange={this.handleChange} type="text" />
-          <button type="submit">Send</button>
+        <form action="#" className="form">
+          <input onChange={this.handleChange} type="text" value={newTask} />
+          <button type="submit">
+            <FaPlus />
+          </button>
         </form>
       </div>
     );
